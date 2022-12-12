e22501e7fc1c64ca5b0bd135100d85154c5d7147
David Aarhus <davidaarhus@Davids-MBP.attlocal.net>
2022-12-12 14:44:13 -0600

    version v4

--- src/components/Tutorial.js
+++ src/components/Tutorial.js
@@ -3,13 +3,20 @@ import { useFrontContext } from '../providers/frontContext';
 function Tutorial() {
   const context = useFrontContext();
 
-  const user = (context.teammate && context.teammate.name) ? context.teammate.name : '';
-  const recipient = (context.conversation && context.conversation.recipient && context.conversation.recipient.name) ? context.conversation.recipient.name : '';
+  const user = (context.teammate && context.teammate.name) ? context.teammate.name : 'NULL';
+  const recipient = (context.conversation && context.conversation.recipient && context.conversation.recipient.name) ? context.conversation.recipient.name : 'Nobody';
+  const subjectline = (context.conversation && context.conversation.subject ) ? context.conversation.subject : ' ';
+//   max characters are 201 for blurb
+  const blurb = (context.conversation && context.conversation.blurb ) ? context.conversation.blurb : ' ';
 
   return (
     <div className="App">
       <p>Hello {user}!</p>
-      <p>You are working with {recipient}!</p>
+      <p>You are working with {recipient}</p>
+      <p>Subject: {subjectline}!</p>
+      <p>Blurb: {blurb}!</p>
+
+
     </div>
   );
 }
